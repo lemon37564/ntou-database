9267a53848fa0bc1030d5ee57aa3766db90f4c76
andy155224 <60310940+andy155224@users.noreply.github.com>
2020-12-19 00:51:53 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,52 +1,41 @@
-<html>
-    <head>
-    <title>首頁</title>
-        <style>
-        body {
-            font-size:16px;font-family:"微軟正黑體";
-            background-image: url('https://i.imgur.com/ez4ikxc.jpg');
-            background-repeat:no-repeat;
-            background-color: #30FFFF;
-            background-position: center;
-            background-size: cover;
-        }
-        </style>
-    </head>
-    <body>
-        <td width=120 valign=top> 
-        <table width=120 border=0>
-        <h2>
-        <form action = "delete.php" method = "post">
-            請輸入學號:<input type = "text" name = "sID">
-        <input type="submit" name = "submit" value = "刪除"><br><br>
-        </form>
-        <form action = "update.php" method = "post">
-            請輸入學號:<input type = "text" name = "sID">
-        <input type="submit" name = "submit" value = "修改">
-        </form>
-
-        <form action = "insert.php" method = "post">
-        <br>
-        新增評鑑<br>
-            學號:<input type = "text" name = "sID"><br>
-            課號:<input type = "text" name = "course_id"><br>
-            課名:<input type = "text" name = "course_name"><br>
-            教授姓名:<input type = "text" name = "prof_name"><br>
-            評分:<input type = "text" name = "rating"><br>
-            評價:<input type = "text" name = "comment"><br>
-            評分標準:<input type = "text" name = "grading_policy"><br>
-            是否有出席成績:<input type = "text" name = "takes_attendance"><br>
-            是否有考古題:<input type = "text" name = "previous_years_test_question">
-        <input type="submit" name = "submit" value = "送出">
-        </form>
-
-        <form action = "select.php" method = "post">
-        <br>
-            依課名查詢:<input type = "text" name = "course_name">
-        <input type="submit" name = "submit" value = "送出">
-        </form>
-        </h2>
-        </table>
-        </td>
-    </body>
+<p> 
+</p><pre name="code" class="html"> 
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <title>課程評鑑首頁</title> 
+        <style type="text/css"> 
+                form{ 
+                    text-align: center; 
+                } 
+        </style> 
+    </head> 
+    <body> 
+        <form action="verification_2.php" method="post" onsubmit="return enter()">
+                帳號<input type="text" name="account" required="required" id="account">
+                密碼<input type="password" name="password" required="required" id="password">
+                                <input type="button" value="註冊" onclick="register();"><input type="submit" value="登入">  
+        </form> 
+        <script type="text/javascript">
+            function enter() {
+                var account = document.getElementById("account").value;//獲取使用者名稱 
+                var password = document.getElementById("password").value;
+                var regex = /^[/s] $/;//宣告一個判斷使用者名稱前後是否有空格
+                if (regex.test(account)) {//判定使用者名稱的是否前後有空格或者使用者名稱是否為空 
+                    alert("帳號格式不合法");
+                    return false;
+                }
+                if (regex.test(password)) {//同上述內容  
+                    alert("密碼格式不合法");
+                    return false;
+                }
+                return true;
+            }
+            function register() {
+                location.href = "register_2.html";//跳轉到註冊頁面 
+            }
+        </script> 
+    </body> 
 </html>
+<!--完成-->
